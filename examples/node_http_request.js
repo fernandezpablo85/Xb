6ec3fddfd61f22300a9f71ff2c720a4358fc246d
Pablo Fernandez <fernandezpablo85@gmail.com>
2011-11-06 20:49:55 -0300

    added nodejs http client example

---
+++ examples/node_http_request.js
@@ -0,0 +1,31 @@
+var http = require('http')
+
+var options = {
+  host: 'isitchristmas.com',
+  port: 80,
+  path: '/'
+};
+
+function checkChristmas(backoff) {
+  http.get(options, function(res) {
+    res.on('data', function(data) {
+      var html = data.toString();
+
+      // exctract the answer
+      var christmas = html.match(/id="answer">\s*(\S*)\s*<\/a>/)[1];
+
+      // If it's not Christmas, we apply the backoff policy.
+      if (christmas === "NO") {
+        console.log("Not Christmas yet, will check in a while");
+      } else {
+        console.log("Merry Christmas!");
+
+        // reset the backoff policy.
+        backoff.reset();
+      }
+
+    });
+  });
+}
+
+Xb.do(checkChristmas);
