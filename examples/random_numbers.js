707f2fc67cdad3cada455c89e859e2de0ab98104
Pablo Fernandez <fernandezpablo85@gmail.com>
2011-11-07 14:01:50 -0300

    random number example updated to run with node

--- examples/random_numbers.js
+++ examples/random_numbers.js
@@ -1,3 +1,8 @@
+// Check if we're running this with node and require xb
+if (typeof require !== "undefined") {
+  var Xb = require('xb');
+}
+
 Xb.do(function (backoff) {
   var oneToTen = Math.floor(Math.random() * 11);
 
